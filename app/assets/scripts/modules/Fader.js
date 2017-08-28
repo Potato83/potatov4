83d3d5872c5e241f5ea4162f6684ca4d8863a718
Sam Vipond <sammy.potato@gmail.com>
2017-08-28 15:15:27 -0400

    just looking

--- app/assets/scripts/modules/Fader.js
+++ app/assets/scripts/modules/Fader.js
@@ -1,10 +1,10 @@
 import $ from 'jquery';
 
 $(document).ready(function() {
- 
+ 	
 	var $bg = $('.o-banner__bg');
-	var backgroundImage = window.getComputedStyle($bg[0], null).backgroundImage;
-	var image = backgroundImage.match(/url\((.*?)\)/)[1];
+	var backgroundImage = window.getComputedStyle($bg[0], null).backgroundImage;	
+	var image = backgroundImage.match(/url\((.*?)\)/)[1];	
 			image = image.replace(/"/g, "");
 			var sprite = new Image();
 			sprite.src = image;
